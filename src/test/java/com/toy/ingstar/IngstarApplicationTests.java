847c7bf3f3f095d6a7d4522ef30103a0a041c495
purplecode53 <purplecode53@gmail.com>
2024-08-28 23:18:17 +0900

    first commit

---
+++ src/test/java/com/toy/ingstar/IngstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.ingstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
